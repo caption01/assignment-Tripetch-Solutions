3d5b361c326a8c4be80799e94dbacf66c59ad7a9
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-11-06 21:21:19 +0700

    add second block and refactor styling

--- src/views/app/Main.tsx
+++ src/views/app/Main.tsx
@@ -34,9 +34,13 @@ function Main() {
 
   const [current, setCurrent] = useState({
     ATHLETS: 0,
+    PLAYERS: 0,
   });
 
+  const althletsContents = contents['ATHLETS'];
   const althletsSrc = AlthletsImageMap[screen];
+
+  const playersContents = contents['PLAYERS'];
   const playerSrc = PlayerImageMap[screen];
 
   const isMobile = screen === PHONE;
@@ -71,46 +75,102 @@ function Main() {
   );
 
   return (
-    <div className="relative grid grid-cols-1 gap-0">
-      <div
-        className="
-          bg-white pt-[2.4rem] pb-[0rem] pl-[50%]
-          md:pt-[8rem] md:pb-[0rem] md:pl-[30%]
+    <>
+      {/* ATHLETS Section */}
+      <div className="relative grid grid-cols-1 gap-0">
+        <div
+          className="
+            bg-white pt-[2.4rem] pb-[0rem] pl-[50%]
+            md:pt-[8rem] md:pb-[0rem] md:pl-[40%]
+            sm:grid sm:p-[3rem] sm:pl-[3rem] sm:mb-[24rem]
+          "
+        >
+          <Headline>ATHLETS</Headline>
+        </div>
+        <div
+          className="absolute z-10 h-full
+            top-[8rem] left-[0rem] w-[70rem]
+            md:top-[8rem] md:-left-[10rem] md:w-[50rem]
+            sm:w-full sm:h-[30rem] sm:top-[11rem]
+          "
+        >
+          <Image src={althletsSrc} alt="AlthletsImage" fill style={{ objectFit: 'contain' }} />
+        </div>
+        <div className="relative sm:flex sm:w-full sm:h-[30rem] sm:overflow-hidden sm:z-0" {...bind('ATHLETS')}>
+          {map(althletsContents, (block, index) => {
+            const isActive = !isMobile || index === current['ATHLETS'];
+            return (
+              isActive && (
+                <BlockContent
+                  key={block.id}
+                  position="right"
+                  config={block.config}
+                  topic={block.topic}
+                  content={block.content}
+                />
+              )
+            );
+          })}
+          {isMobile ? (
+            <div className="absolute bottom-[10%] left-[50%] -translate-x-[50%] flex gap-[1rem]">
+              {map(althletsContents, (block, index) => {
+                const isActive = index === current['ATHLETS'];
+                const dotColorConfig = block.topic.config.dotColor;
+                const dotColor = !isActive ? 'bg-grey' : dotColorConfig;
+                return <div key={index} className={`w-[1rem] h-[1rem] ${dotColor} rounded-full`} />;
+              })}
+            </div>
+          ) : null}
+        </div>
+      </div>
+      {/* PLAYERS Section */}
+      <div className="relative grid grid-cols-1 gap-0">
+        <div
+          className="
+          bg-white pt-[12.4rem] pb-[0rem] pl-[10%]
+          md:pt-[5rem] md:pb-[0rem] md:pl-[5%]
           sm:grid sm:p-[3rem] sm:pl-[3rem] sm:mb-[24rem]
         "
-      >
-        <Headline>ATHLETS</Headline>
+        >
+          <Headline>PLAYERS</Headline>
+        </div>
+        <div
+          className="absolute z-10 h-full
+            top-[0rem] right-[0rem] w-[70rem]
+            md:-top-[5rem] md:right-[0rem] md:w-[50rem]
+            sm:w-full sm:h-[30rem] sm:top-[11rem]
+          "
+        >
+          <Image src={playerSrc} alt="PlayersImage" fill style={{ objectFit: 'contain' }} />
+        </div>
+        <div className="relative sm:flex sm:w-full sm:h-[30rem] sm:overflow-hidden sm:z-0" {...bind('PLAYERS')}>
+          {map(playersContents, (block, index) => {
+            const isActive = !isMobile || index === current['PLAYERS'];
+            return (
+              isActive && (
+                <BlockContent
+                  key={block.id}
+                  position="left"
+                  config={block.config}
+                  topic={block.topic}
+                  content={block.content}
+                />
+              )
+            );
+          })}
+          {isMobile ? (
+            <div className="absolute bottom-[10%] left-[50%] -translate-x-[50%] flex gap-[1rem]">
+              {map(althletsContents, (block, index) => {
+                const isActive = index === current['PLAYERS'];
+                const dotColorConfig = block.topic.config.dotColor;
+                const dotColor = !isActive ? 'bg-grey' : dotColorConfig;
+                return <div key={index} className={`w-[1rem] h-[1rem] ${dotColor} rounded-full`} />;
+              })}
+            </div>
+          ) : null}
+        </div>
       </div>
-      <div
-        className="
-          absolute top-[8rem] left-0 w-1/2 h-full z-10
-          md:w-1/2 md:-left-[10%]
-          sm:w-full sm:h-[30rem] sm:top-[11rem]
-        "
-      >
-        <Image src={althletsSrc} alt="AlthletsImage" fill style={{ objectFit: 'contain' }} />
-      </div>
-      <div className="relative sm:flex sm:w-full sm:h-[30rem] sm:overflow-hidden sm:z-0" {...bind('ATHLETS')}>
-        {map(contents.ATHLETS, (block, index) => {
-          const isActive = !isMobile || index === current['ATHLETS'];
-          return (
-            isActive && (
-              <BlockContent key={block.id} config={block.config} topic={block.topic} content={block.content} />
-            )
-          );
-        })}
-        {isMobile ? (
-          <div className="absolute bottom-[10%] left-[50%] -translate-x-[50%] flex gap-[1rem]">
-            {map(contents.ATHLETS, (block, index) => {
-              const isActive = index === current['ATHLETS'];
-              const dotColorConfig = block.topic.config.dotColor;
-              const dotColor = !isActive ? 'bg-grey' : dotColorConfig;
-              return <div key={index} className={`w-[1rem] h-[1rem] ${dotColor} rounded-full`} />;
-            })}
-          </div>
-        ) : null}
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/views/app/components/BlockContent.tsx
+++ src/views/app/components/BlockContent.tsx
@@ -1,6 +1,7 @@
 import { Topic, Content } from './common';
 
 type BlockContent = {
+  position: string;
   config?: any;
   topic?: ContentProps;
   content?: ContentProps;
@@ -12,17 +13,24 @@ type ContentProps = {
   config?: any;
 };
 
-function BlockContent({ config = {}, topic, content }: BlockContent) {
+function BlockContent({ position = 'right', config = {}, topic, content }: BlockContent) {
   const { bgColor } = config;
 
+  const layout: Record<string, string> = {
+    right: `${bgColor} pt-[6rem] pb-[6rem] pl-[50%] 
+      md:pt-[5rem] md:pb-[6rem] md:pl-[40%]
+      sm:px-[3rem] sm:py-[6rem] sm:w-full sm:h-full
+      `,
+    left: `${bgColor} pt-[6rem] pb-[6rem] pl-[10%]
+      md:pt-[5rem] md:pb-[6rem] md:pl-[5%]
+      sm:px-[3rem] sm:py-[6rem] sm:w-full sm:h-full
+      `,
+  };
+
+  const layoutPosition = layout[position];
+
   return (
-    <div
-      className={`
-        ${bgColor} pt-[6rem] pb-[6rem] pl-[50%]
-        md:pt-[5rem] md:pb-[6rem] md:pl-[30%]
-        sm:px-[3rem] sm:py-[6rem] sm:w-full sm:h-full
-        `}
-    >
+    <div className={layoutPosition}>
       {topic ? (
         <div className="mb-[2.1rem]">
           <Topic order={topic.order} config={topic.config}>
@@ -31,7 +39,7 @@ function BlockContent({ config = {}, topic, content }: BlockContent) {
         </div>
       ) : null}
       {content ? (
-        <div>
+        <div className="max-w-[120ch] md:max-w-[80ch]">
           <Content config={content.config}>{content.body}</Content>
         </div>
       ) : null}
